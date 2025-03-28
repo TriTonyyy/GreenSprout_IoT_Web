ad791c4b6ba8b5ec90b9df4861f60bebf0f80511
Kien <kienchau241@gmail.com>
2025-03-28 12:58:08 +0700

    Them local URL

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASEURL = 'https://capstone-project-iot-1.onrender.com'  ;
+const BASEURL = 'https://capstone-project-iot-1.onrender.com';
+const LOCAL = `http://localhost:8000`;
 
 const axiosClient = axios.create({
     baseURL: BASEURL,
