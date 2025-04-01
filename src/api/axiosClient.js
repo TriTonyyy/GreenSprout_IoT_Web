71ce4e0247d372b799376f6ba814e4e533e8d59e
Kien <kienchau241@gmail.com>
2025-04-01 12:31:36 +0700

    [ADD] add token helper

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,12 +1,17 @@
 import axios from "axios";
+import { getToken, removeToken } from "../helper/tokenHelper";
 
 const BASEURL = 'https://capstone-project-iot-1.onrender.com';
 const LOCAL = `http://localhost:8000`;
+
+let token;
+getToken().then((res) => (token = res));
 
 const axiosClient = axios.create({
     baseURL: BASEURL,
     headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
     },
 })
 
