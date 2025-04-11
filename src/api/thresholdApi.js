20cddfd6e2a65398b18be23699a06ee0239862b0
thangnt045 <thangnguyen0405@gmail.com>
2025-04-11 16:06:45 +0700

    fetched successfully threshold data

--- src/api/thresholdApi.js
+++ src/api/thresholdApi.js
@@ -1,11 +1,11 @@
 import axiosClient from "./axiosClient";
 
-// Function to update threshold by device ID
+// Function to update specific sensor threshold by control ID
 export const updateThreshold = async (params) => {
-  const { id_esp, scheduleId, data } = params;
-  console.log(id_esp, scheduleId, data);
+  const { id_esp, controlId, data } = params;
+  console.log(id_esp, controlId, data);
   const res = await axiosClient.put(
-    `/api/control/updateControls/${id_esp}/${scheduleId}`,
+    `/api/control/updateControls/${id_esp}/${controlId}`,
     data
   );
   return res.data;
