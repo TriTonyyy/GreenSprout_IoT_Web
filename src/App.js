3c807ea8c246d1a8ca31a270676118bfb48d77c7
Kien <kienchau241@gmail.com>
2025-04-16 18:16:51 +0700

    [Fix] commit to merge branch

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ function App ()  {
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/garden/:gardenId" element={<DetailedGardenPage />} />
-        <Route path="/schedule" element={<SchedulePage />} />
+        {/* <Route path="/schedule" element={<SchedulePage />} /> */}
         <Route path="/account" element={<AccountPage />} />
       </Route>
     </Routes>
