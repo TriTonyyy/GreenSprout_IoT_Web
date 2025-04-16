5821343cd1447fac24cf5d220795f70dd7fe1006
thangnt045 <thangnguyen0405@gmail.com>
2025-04-16 21:42:10 +0700

    change name file for consistent

--- src/pages/account/AccountPage.jsx
+++ src/pages/account/AccountPage.jsx
@@ -1,179 +1,226 @@
-import React, {useState, useEffect} from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import HeaderComponent from '../../components/Header/HeaderComponent'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import HeaderComponent from "../../components/Header/HeaderComponent";
 import { useNavigate } from "react-router";
-import { getUserInfoAPI } from '../../api/authApi';
-import { updateAvatarAPI, updateProfileApi } from '../../api/userApi';
-import {apiResponseHandler, areUSurePopup, changePasswordPopUp} from '../../components/Alert/alertComponent';
-import SideNavigationBar from '../../components/SideNavigationBar/SideNavigationBar';
-import FooterComponent from '../../components/FooterComponent/FooterComponent';
-import i18n from '../../i18n';
-import { setLanguage } from '../../redux/Reducers/langReducer';
-import { getLang } from '../../redux/selectors/langSelectors';
+import { changePasswordAPI, getUserInfoAPI } from "../../api/authApi";
+import { updateAvatarAPI, updateProfileApi } from "../../api/userApi";
+import {
+  apiResponseHandler,
+  areUSurePopup,
+  changePasswordPopUp,
+} from "../../components/Alert/alertComponent";
+import SideNavigationBar from "../../components/SideNavigationBar/SideNavigationBar";
+import FooterComponent from "../../components/FooterComponent/FooterComponent";
+import i18n from "../../i18n";
+import { setLanguage } from "../../redux/Reducers/langReducer";
+import { getLang } from "../../redux/selectors/langSelectors";
 
 export default function AccountPage() {
-    const dispatch = useDispatch();
-    const lang = useSelector(getLang);
-    const [name, setName] = useState('');
-    const [avatar, setAvatar] = useState('');
-    const [fileUpload, setFileUpload] = useState('');
+  const dispatch = useDispatch();
+  const lang = useSelector(getLang);
+  const [name, setName] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [fileUpload, setFileUpload] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState("");
-    const [userInfo, setUserInfo] = useState({});
-    const navigate = useNavigate();
-    
-    const saveUpdateUserInfo = ()=>{
-        updateProfileApi({name, email, avatar})
-            .then((res)=>{
-                console.log(res, "res");
-                setUserInfo(res.data);
-                alert(res.message)
-                window.location.reload();
-            })
-            .catch((err)=>{
-                apiResponseHandler(err.response.data.message);
-            })  
-        updateAvatarAPI(fileUpload)
-            .then((res)=>{
-                console.log(res,'res');
-                
-            })
-            .catch((err)=>{
-                console.log(err);
-                
-            })
-        
-    }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userInfo, setUserInfo] = useState({});
+  const navigate = useNavigate();
 
-    const changeLanguage = async (language)=>{
-        console.log(language);
-        areUSurePopup(i18n.t("change-lang-mess"))
-            .then((res)=>{
-                i18n.changeLanguage(language)
-                    .then((t)=>{
-                        t('key')
-                        dispatch(setLanguage(language));
-                    })
-            })
-            .catch(()=>{})   
-    }
+  const saveUpdateUserInfo = () => {
+    updateProfileApi({ name, email, avatar })
+      .then((res) => {
+        console.log(res, "res");
+        setUserInfo(res.data);
+        alert(res.message);
+        window.location.reload();
+      })
+      .catch((err) => {
+        apiResponseHandler(err.response.data.message);
+      });
+    updateAvatarAPI(fileUpload)
+      .then((res) => {
+        console.log(res, "res");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const changePassword = async ()=>{
-        await changePasswordPopUp()
-            .then((res)=>{
-                changePasswordAPI({
-                    currentPassword:res.oldPassword,
-                    newPassword:res.password
-                })
-                    .then((res)=>{
-                        apiResponseHandler(res.message)
-                    })
-                    .catch((err)=>{
-                        apiResponseHandler(err.message)
-                    })
-            })
-    }
+  const changeLanguage = async (language) => {
+    console.log(language);
+    areUSurePopup(i18n.t("change-lang-mess"))
+      .then((res) => {
+        i18n.changeLanguage(language).then((t) => {
+          t("key");
+          dispatch(setLanguage(language));
+        });
+      })
+      .catch(() => {});
+  };
 
-    const handleImgUpload=async(e)=>{
-        const file = e.target.files[0];
-        console.log(file, "file");
-        
-        if(file){
-            const reader = new FileReader();
-            reader.onload = (e)=>{
-                const base64 = e.target.result;
-                setAvatar(base64)
-                setFileUpload(base64.split(',')[1])
-            }
-            reader.readAsDataURL(file)
-        }   
-    }
-
-    useEffect(()=>{
-        getUserInfoAPI()
-        .then((res)=>{
-            setUserInfo(res.data);
+  const changePassword = async () => {
+    await changePasswordPopUp().then((res) => {
+      changePasswordAPI({
+        currentPassword: res.oldPassword,
+        newPassword: res.password,
+      })
+        .then((res) => {
+          apiResponseHandler(res.message);
         })
-        .catch((err)=>{
-            console.log(err);
-        })
-    }, [])
-    useEffect(()=>{
-        setEmail(userInfo?.email ? userInfo.email : '');
-        setName(userInfo?.name ? userInfo.name : '');
+        .catch((err) => {
+          apiResponseHandler(err.message);
+        });
+    });
+  };
 
-    }, [userInfo])
+  const handleImgUpload = async (e) => {
+    const file = e.target.files[0];
+    console.log(file, "file");
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const base64 = e.target.result;
+        setAvatar(base64);
+        setFileUpload(base64.split(",")[1]);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  useEffect(() => {
+    getUserInfoAPI()
+      .then((res) => {
+        setUserInfo(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  useEffect(() => {
+    setEmail(userInfo?.email ? userInfo.email : "");
+    setName(userInfo?.name ? userInfo.name : "");
+  }, [userInfo]);
 
   return (
     <div>
-        <HeaderComponent/>
-        <div className='flex'>
-            <SideNavigationBar/>
-            <div className='mx-10 my-5 justify-between flex-col w-full'>
-                <h1 className='text-3xl'><strong>{i18n.t("account")}</strong></h1>
-                <div className='flex pt-5  justify-between items-center '>
-                    <div className='flex'>
-                        <img src={avatar !== "" ? avatar : require("../../assets/images/AvatarDefault.png")} className='py-5 max-w-lg' alt='avatar'/>
-                        {avatar === ''? 
-                            <h2 className='text-2xl p-4'>
-                                Ảnh cá nhân <br/>
-                                PNG, JPEG dưới 5MB <br/>
-                            </h2>: <></>}
-                        
-                    </div>
-                    {/* <button className='bg-gray-300 p-5 rounded-2xl text-xl border-2 border-gray-500 hover:bg-gray-400'>
+      <HeaderComponent />
+      <div className="flex">
+        <SideNavigationBar />
+        <div className="mx-10 my-5 justify-between flex-col w-full">
+          <h1 className="text-3xl">
+            <strong>{i18n.t("account")}</strong>
+          </h1>
+          <div className="flex pt-5  justify-between items-center ">
+            <div className="flex">
+              <img
+                src={
+                  avatar !== ""
+                    ? avatar
+                    : require("../../assets/images/AvatarDefault.png")
+                }
+                className="py-5 max-w-lg"
+                alt="avatar"
+              />
+              {avatar === "" ? (
+                <h2 className="text-2xl p-4">
+                  Ảnh cá nhân <br />
+                  PNG, JPEG dưới 5MB <br />
+                </h2>
+              ) : (
+                <></>
+              )}
+            </div>
+            {/* <button className='bg-gray-300 p-5 rounded-2xl text-xl border-2 border-gray-500 hover:bg-gray-400'>
                         Tải hình ảnh lên
                     </button> */}
 
-                    <input type='file' onChange={(e)=>handleImgUpload(e)}/>
-                </div>
+            <input type="file" onChange={(e) => handleImgUpload(e)} />
+          </div>
 
-                <div className='pt-5  justify-between items-center border-b-2 pb-4 '>
-                    <h2 className='text-2xl '><strong>{i18n.t("fullname")}</strong></h2>
-                    <input type='text' className='border-2 border-gray-300 p-2 mt-2 mr-2 mb-2 rounded-lg bg-gray-100 w-full' placeholder='Nhập họ và tên' value={name} onChange={(e)=>setName(e.target.value)}/>
-                </div>
-                
-                <div className='pt-5  justify-between items-center border-b-2 pb-4 '>
-                    <h2 className='text-2xl'><strong>Email</strong></h2>
-                    <input readOnly type='email' className='border-2 border-gray-300 p-2 mt-2 mr-2 mb-2 rounded-lg bg-gray-100 w-full' placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                </div>
+          <div className="pt-5  justify-between items-center border-b-2 pb-4 ">
+            <h2 className="text-2xl ">
+              <strong>{i18n.t("fullname")}</strong>
+            </h2>
+            <input
+              type="text"
+              className="border-2 border-gray-300 p-2 mt-2 mr-2 mb-2 rounded-lg bg-gray-100 w-full"
+              placeholder="Nhập họ và tên"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
 
-                <div className='pt-5  justify-between items-center border-b-2 pb-4 '>
-                    <h2 className='text-2xl '><strong>{i18n.t('password')}</strong></h2>
-                    <input readOnly type='password' className='border-2 border-gray-300 p-2 mt-2 mr-2 mb-2 rounded-lg bg-gray-100 w-full' placeholder='Mật Khẩu' value={"abcdefgh"} onChange={(e)=>setPassword(e.target.value)}/>
-                    <button className='p-4 rounded bg-green-600 mt-4' onClick={changePassword}>
-                        <p className='text-white text-2xl'>{i18n.t("change-password")}</p>
-                    </button>
-                </div>
+          <div className="pt-5  justify-between items-center border-b-2 pb-4 ">
+            <h2 className="text-2xl">
+              <strong>Email</strong>
+            </h2>
+            <input
+              readOnly
+              type="email"
+              className="border-2 border-gray-300 p-2 mt-2 mr-2 mb-2 rounded-lg bg-gray-100 w-full"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-                <div className='pt-5  justify-between items-center border-b-2 pb-4 '>
-                    <h2 className='text-2xl '><strong>{i18n.t("lang")}</strong></h2>
-                    <select 
-                        className='border-2 border-gray-300 p-2 mt-2 mr-2 mb-2 rounded-lg bg-gray-100 w-full' 
-                        onChange={(e) => changeLanguage(e.target.value)}
-                    >
-                        {lang === "vi" ? (
-                            <div>
-                                <option selected  value="vi">Tiếng Việt</option>
-                                <option  value="en" >English</option>
-                            </div>
-                        ) :(
-                            <div>
-                                <option  value="vi">Tiếng Việt</option>
-                                <option selected  value="en" >English</option>
-                            </div>
-                        )}
-                        
-                    </select>
-                </div>
+          <div className="pt-5  justify-between items-center border-b-2 pb-4 ">
+            <h2 className="text-2xl ">
+              <strong>{i18n.t("password")}</strong>
+            </h2>
+            <input
+              readOnly
+              type="password"
+              className="border-2 border-gray-300 p-2 mt-2 mr-2 mb-2 rounded-lg bg-gray-100 w-full"
+              placeholder="Mật Khẩu"
+              value={"abcdefgh"}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              className="p-4 rounded bg-green-600 mt-4"
+              onClick={changePassword}
+            >
+              <p className="text-white text-2xl">{i18n.t("change-password")}</p>
+            </button>
+          </div>
 
-                <button className='w-full p-4 rounded bg-blue-400' onClick={saveUpdateUserInfo}>
-                    <p className='text-white text-2xl'>{i18n.t("save")}</p>
-                </button>
-            </div>
+          <div className="pt-5  justify-between items-center border-b-2 pb-4 ">
+            <h2 className="text-2xl ">
+              <strong>{i18n.t("lang")}</strong>
+            </h2>
+            <select
+              className="border-2 border-gray-300 p-2 mt-2 mr-2 mb-2 rounded-lg bg-gray-100 w-full"
+              onChange={(e) => changeLanguage(e.target.value)}
+            >
+              {lang === "vi" ? (
+                <div>
+                  <option selected value="vi">
+                    Tiếng Việt
+                  </option>
+                  <option value="en">English</option>
+                </div>
+              ) : (
+                <div>
+                  <option value="vi">Tiếng Việt</option>
+                  <option selected value="en">
+                    English
+                  </option>
+                </div>
+              )}
+            </select>
+          </div>
+
+          <button
+            className="w-full p-4 rounded bg-blue-400"
+            onClick={saveUpdateUserInfo}
+          >
+            <p className="text-white text-2xl">{i18n.t("save")}</p>
+          </button>
         </div>
-        <FooterComponent/>
+      </div>
+      <FooterComponent />
     </div>
-  )
+  );
 }
