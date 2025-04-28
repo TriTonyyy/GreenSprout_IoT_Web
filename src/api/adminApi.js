23d66069cbe059edd9f0537dafc6548489889f0b
Kien <kienchau241@gmail.com>
2025-04-28 12:20:43 +0700

    Add user management functionality with user cards and pagination

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -3,3 +3,8 @@ export const getAllDeviceApi = async (page,limit) => {
     const response = await axiosClient.get(`/api/admin/getAllDevice?page=${page}&limit=${limit}`);
     return response.data;  // Return the data from the API response
 };
+
+export const getAllUserApi = async (page,limit) => {
+    const response = await axiosClient.get(`/api/admin/getAllUser?page=${page}&limit=${limit}`);
+    return response.data;  // Return the data from the API response
+};
